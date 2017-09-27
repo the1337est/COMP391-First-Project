1f65f0b2212b1a3e53a3a55f5297f4dcf27dc181
Gaurav <the1337est@live.com>
2017-09-27 11:49:15 -0400

    -Changed bounds to Vector2 variables

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     public float Speed;
 
+    public Vector2 Min, Max;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,16 +18,14 @@ public class PlayerController : MonoBehaviour
 	void FixedUpdate ()
     {
 
-
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
         Vector2 tempPos = rb.position;
 
         tempPos +=  Vector2.up * vertical * Time.deltaTime * Speed;
         tempPos += Vector2.right * horizontal * Time.deltaTime * Speed;
-
-        tempPos.x = Mathf.Clamp(tempPos.x, -8f, 8f);
-        tempPos.y = Mathf.Clamp(tempPos.y, -4.5f, 4.5f);
+        tempPos.x = Mathf.Clamp(tempPos.x, Min.x, Max.x);
+        tempPos.y = Mathf.Clamp(tempPos.y, Min.y, Max.y);
 
         rb.position = tempPos;
     }
